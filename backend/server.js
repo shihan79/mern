2328ead73051aaf9ed5380d20756913a3f4c7808
shihan <shihanaust@gmail.com>
2023-11-03 00:48:46 +0600

    first commit

---
+++ backend/server.js
@@ -0,0 +1,33 @@
+// require('dotenv').config()
+
+const express = require('express')
+const mongoose = require('mongoose')
+const workoutRoutes = require('./routes/workouts')
+const userRoutes = require('./routes/user')
+
+// express app
+const app = express()
+
+// middleware
+app.use(express.json())
+
+app.use((req, res, next) => {
+  console.log(req.path, req.method)
+  next()
+})
+
+// routes
+app.use('/api/workouts', workoutRoutes)
+app.use('/api/user', userRoutes)
+
+// connect to db
+mongoose.connect('mongodb+srv://shihanaust:5s2MQ6TZFcojmNnN@mernapp.c54ocgz.mongodb.net/')
+  .then(() => {
+    // listen for requests
+    app.listen(4000, () => {
+      console.log('connected to db & listening on port', process.env.PORT)
+    })
+  })
+  .catch((error) => {
+    console.log(error)
+  })
